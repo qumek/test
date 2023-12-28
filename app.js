d71fe9dc8547c6318c1ec079a06b63cea651b80f
qumek <kumekovtimur@gmail.com>
2023-12-29 02:54:53 +0600

    2.3

--- app.js
+++ app.js
@@ -2,14 +2,38 @@
 // firstName = 'vova'
 // console.log('salam', firstName)
 
-let firstName = 'Тимур'
-firstName = 'Тима'
+// let firstName = 'Тимур'
+// firstName = 'Тима'
 
-let age = '32'
-let city = 'Астана'
-let hobby = 'Прокрастинация'
+// let age = '32'
+// let city = 'Астана'
+// let hobby = 'Прокрастинация'
 
-console.log('Имя:', firstName)
-console.log('Возраст:', age)
-console.log('Город:', city)
-console.log('Хобби:', hobby)
+// console.log('Имя:', firstName)
+// console.log('Возраст:', age)
+// console.log('Город:', city)
+// console.log('Хобби:', hobby)
+
+// Number
+// BigInt
+// String
+// Boolean
+// undefined
+// null
+// Symbol
+// Object
+
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 115;
+let percentage = 21;
+let fullPrice = 2400;
+let responsive = true;
+
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
